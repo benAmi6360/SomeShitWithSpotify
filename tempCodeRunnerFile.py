a335fcc3c89f7897e924f5dd626186616de6c7c2
benAmi6360 <minebenking@gmail.com>
2023-04-06 13:19:50 +0300

    Removed tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-CONFIG = dotenv_values('.env')
-
-scope = ['playlist-read-private', 
-         'playlist-read-collaborative', 
-         'playlist-modify-private', 
-         'playlist-modify-public', 
-         'user-top-read',
-         'user-read-private',
-         'user-read-email']
